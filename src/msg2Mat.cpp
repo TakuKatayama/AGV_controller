a2c1d8ba79c3de0d1ae41efecdb3df225e624500
TakuKatayama <17kmj09@ms.dendai.ac.jp>
2018-11-20 13:23:05 +0900

    error occur. correcting it.

--- src/msg2Mat.cpp
+++ src/msg2Mat.cpp
@@ -5,6 +5,7 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <iostream>
 
 ros::Publisher pub;
 
@@ -12,24 +13,33 @@ void cb(const sensor_msgs::ImageConstPtr& msg)
 {
     cv_bridge::CvImagePtr cv_ptr;
     cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::TYPE_32FC1);
-    cv::Mat msgmat = cv_ptr->image;
-    cv::Mat floor = cv::Mat::zeros(msgmat.rows, msgmat.cols, CV_8U);
+    int width = cv_ptr->image.cols;
+    int height = cv_ptr->image.rows;
+    //cv::Mat depth_map = cv_ptr->image;
+    cv_bridge::CvImagePtr floor_ptr;
+    cv::Mat floor(cv::Size(width, height), CV_8UC1);
 
-    for(int v = 0; v < msgmat.cols; v++)
+    const double fku_l_vga = 349.824;
+    double y = 0;
+    double z = 0;
+    
+    int u_center = height / 2;
+
+    for(int v = 0; v < height; v++)
     {
-        for(int u = 0; u < msgmat.rows; u++)
+        for(int u = 0; u < width; u++)
         {
-            double y = msgmat.ptr<double>(u)[v] * 1;
-            if (y < 0.5)
-            {
-                floor.ptr<uchar>(u)[v] = 128;
-            }
+            z = cv_ptr->image.data[u + v * height];
+            y = z * (u_center - v) / fku_l_vga;
+            if (y < 0) {floor.at<unsigned char>(v, u) = 100;}
+            else {floor.at<unsigned char>(v, u) = 0;}
         }
     }    
 
-    sensor_msgs::ImagePtr pub_ptr = cv_bridge::CvImage(std_msgs::Header(), sensor_msgs::image_encodings::MONO8, floor).toImageMsg();
-
-    pub.publish(pub_ptr); 
+    cv::imshow("y", floor);
+    cv::waitKey(3);
+    sensor_msgs::ImagePtr pub_ptr = cv_bridge::CvImage(std_msgs::Header(), sensor_msgs::image_encodings::TYPE_8UC1, floor).toImageMsg();
+    pub.publish(cv_ptr->toImageMsg()); 
 }
 
 int main(int argc, char** argv)
